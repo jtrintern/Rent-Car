3b85bb7f07cf7a6e735d4bc89d231adc0dd1d42e
Radith <122001453+GRadith@users.noreply.github.com>
2023-11-13 09:23:44 +0700

    print

--- resources/views/front-end/layouts/index.blade.php
+++ resources/views/front-end/layouts/index.blade.php
@@ -43,7 +43,6 @@
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.7.1/jquery.min.js"></script>
   <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.bundle.min.js"></script>
   <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
-
   <!-- Template Main CSS File -->
   <link href="{{ asset('rentCar/assets/css/style.css') }}" rel="stylesheet">
 </head>
